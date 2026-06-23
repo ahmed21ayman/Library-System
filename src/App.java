a2779cd223095bbe562fcc5be8926d14efe5e738
Ahmed Ayman Khalil <ahmedaymen10111@gmail.com>
2026-06-23 22:22:03 +0300

    v1: build parent book

--- src/App.java
+++ src/App.java
@@ -1,5 +1,14 @@
 public class App {
-    
+    static class Book{
+        protected String title;
+        protected String author;
+        protected boolean Availbility;
+        Book(String title, String author, boolean Availbility){
+            this.title = title;
+            this.author = author;
+            this.Availbility = Availbility;
+        }
+    }
 
     public static void main(String[] args) throws Exception {
         
